c735bf48e7d36c2e1554026e79ae5f84ecee3707
elawilkinson <elaw454@gmail.com>
2022-09-14 16:14:52 +0100

    basic components begun, next need to feed down props to try and map through data

---
+++ src/libs/fetchrequests.js
@@ -0,0 +1,14 @@
+// const url = 'https://api-formula-1.p.rapidapi.com/teams/';
+
+// const options = {
+//   method: 'GET',
+//   headers: {
+//     'X-RapidAPI-Key': process.env.REACT_APP_API_KEY,
+//     'X-RapidAPI-Host': 'api-formula-1.p.rapidapi.com'
+//   }
+// };
+
+// fetch(url, options)
+// 	.then(res => res.json())
+// 	.then(json => console.log(json))
+// 	.catch(err => console.error('error:' + err));
