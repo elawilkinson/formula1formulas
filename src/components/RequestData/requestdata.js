57e48a86cfaaf2076bca67d7625c85ee927717ca
elawilkinson <elaw454@gmail.com>
2022-09-14 17:48:39 +0100

    working on next palette of data menu

--- src/components/RequestData/requestdata.js
+++ src/components/RequestData/requestdata.js
@@ -1,15 +1,27 @@
+import React, {useState} from 'react'
 
+import DataMenu from '../DataMenu/datamenu.js'
 
 function RequestData ({setShowRanking}) {
+
+    const[showDataMenu, setShowDataMenu] = useState(false)
 
     function getStats(){
         setShowRanking(true)
     }
 
+    function showOptions(){
+        setShowDataMenu(true)
+    }
+
     return(
         <>
             <p>F O R M U L A</p>
-            <button onClick={getStats}> Get stats </button>
+            <button onClick={getStats}> 2021 rankings </button>
+            <button onClick={showOptions}> More data </button>
+            {showDataMenu ? (
+                <DataMenu />
+            ) : <></>}
         </>
     )
 }
