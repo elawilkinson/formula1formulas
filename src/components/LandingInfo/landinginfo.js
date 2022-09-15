2cffb398543eafebc462cf58afb9d262992ccab0
elawilkinson <elaw454@gmail.com>
2022-09-15 17:04:59 +0100

    documentation begun - README in progress

--- src/components/LandingInfo/landinginfo.js
+++ src/components/LandingInfo/landinginfo.js
@@ -1,7 +1,7 @@
 function LandingInfo (){
     return(
         <section className="landing-text-container">
-            <h1 className="landing-info-text"><span id="app-title-span">formula</span> F O R M U L A S</h1>
+            <h1 className="landing-info-text"><span id="app-title-span">formula 1</span> F O R M U L A S</h1>
             <h3 className="landing-info-subheading">Get your racing numbers fix</h3>
         </section>
     )
