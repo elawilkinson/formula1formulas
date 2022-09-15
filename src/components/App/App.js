002862f781b78ca442261aa8611346ebd1acef6a
elawilkinson <elaw454@gmail.com>
2022-09-15 11:47:37 +0100

    driver and constructor listings coming through

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,43 +1,13 @@
-import React, {useState, useEffect} from "react";
+import React from "react";
 
 import './App.css';
-
 import RequestData from '../RequestData/requestdata.js';
-import StatsDisplay from "../StatsDisplay/statsdisplay.js";
 
-
-function App() {
-
-  const [showRanking, setShowRanking] = useState(false)
-  const [listings, setListings] = useState([])
-  const url = 'https://api-formula-1.p.rapidapi.com/rankings/teams?season=2021';
-
-  const getListings = {
-    method: 'GET',
-    headers: {
-      'X-RapidAPI-Key': process.env.REACT_APP_API_KEY,
-      'X-RapidAPI-Host': 'api-formula-1.p.rapidapi.com'
-    }
-  };
-
-  useEffect(() => {
-    fetch(url, getListings)
-      .then(res => res.json())
-      .then(json => setListings(json.response))
-      .catch(err => console.error('error:' + err));
-  
-  }, [])
- 
-  console.log(listings)
-
-
+function App() {  
   
   return (
     <div className="App">
-      <RequestData listings={listings} setShowRanking={setShowRanking}/>
-      {showRanking ? (
-        <StatsDisplay listings={listings} setShowRanking={setShowRanking}/>
-      ) : <></>}
+      <RequestData/>
     </div>
   );
 }
--- src/components/StatsDisplay/statsdisplay.js
+++ src/components/StatsDisplay/statsdisplay.js
@@ -1,20 +1,37 @@
+import DriverCard from "../DriverCard/drivercard.js";
 import TeamCard from "../TeamCard/teamcard.js";
 
-function StatsDisplay ({listings, setShowRanking}) {
+function StatsDisplay ({teamListings, driverListings, setShowRanking, showCons, showDriver}) {
     
     function closeListings () {
         setShowRanking(false);
     }
 
+    console.log(driverListings)
+
     return(
         <>
-            <div>
-                {listings.map((listing, index) => {
+            {showCons ? (
+
+                <div>
+                {teamListings.map((listing, index) => {
                     return(
                     <TeamCard key={index} listing={listing} />
                     )
                 })}
             </div>
+            ) : <> </> }
+            
+            {showDriver ? (
+            <div>
+                {driverListings.map((listing, index) => {
+                    return(
+                    <DriverCard key={index} listing={listing} />
+                    )
+                })}
+            </div>
+            ) : <> </> }
+            
             <button onClick={closeListings}>Close stats</button>
         </>
     )
