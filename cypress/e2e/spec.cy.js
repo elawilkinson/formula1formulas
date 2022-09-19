e96936db93fbd5b73c78db7fd0aaf123cb7285c2
elawilkinson <elaw454@gmail.com>
2022-09-19 15:01:54 +0100

    cypress tests planned and written - passing

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,44 @@
-describe('My First Test', () => {
-  it('Visits the Kitchen Sink', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+// describe('Constructor rankings test', () => {
+//   it('Loads a list of F1 teams when the Constructor button is pressed', () => {
+//     cy.visit('https://formula1formulas.netlify.app')
+
+//     cy.contains('Constructor').click()
+//   })
+
+//   it('Fetches and returns the correct list', () => {
+//     cy.contains('Mercedes')
+//     cy.contains('Haas')
+//     })
+//   })
+
+  // describe('Driver rankings test', () => {
+  //   it('Loads a list of F1 teams when the Driver button is pressed', () => {
+  //     cy.visit('https://formula1formulas.netlify.app')
+  
+  //     cy.contains('Driver').click()
+  //   })
+  
+  //   it('Fetches and returns the correct list', () => {
+  //     cy.contains('Hamilton')
+  //     cy.contains('Nikita')
+  //     })
+  //   })
+  
+    describe('Testing the deeper data dive charts', () => {
+      it('Loads a list of F1 teams when the Driver button is pressed', () => {
+        cy.visit('https://formula1formulas.netlify.app')
+    
+        cy.contains('dives').click()
+      })
+    
+      it('Shows three dataset options, which the user can click on', () => {
+        cy.contains('Pole').click()
+        cy.contains('Laptimes').click()
+        cy.contains('grids').click()
+        })
+
+      it('Returns a visual chart', () => {
+        cy.get('canvas').should('be.visible')
+        })
+      })
+    
---
+++ planning/testingplan.md
@@ -0,0 +1,20 @@
+## USER JOURNEY TESTING
+
+### 1. A user can retrieve correct constructor rankings
+a) GIVEN a user visits formula1formulas.netlify.app, `- DONE, PASSING` 
+b) WHEN they click on '2021 | constructor rankings' `- DONE, PASSING` 
+c) THEN the list of F1 teams should appear, in rank order. `- DONE, PASSING` 
+    AND this list should contain 'Mercedes' and 'Haas'.  `- DONE, PASSING` 
+
+### 2. A user can retrieve driver rankings
+a) GIVEN a user visits formula1formulas.netlify.app, `- DONE, PASSING` 
+b) WHEN they click on '2021 | driver rankings' `- DONE, PASSING` 
+c) THEN the list of F1 drivers should appear, in rank order. `- DONE, PASSING` 
+    AND this list should contain 'Hamilton'. `- DONE, PASSING` 
+
+### 3. A user can load multiple data charts
+a) GIVEN a user visits formula1formulas.netlify.app, `- DONE, PASSING` 
+b) WHEN they click on 'Data dives' `- DONE, PASSING` 
+c) THEN three further options should appear `- DONE, PASSING` 
+   AND these should display a chart `- DONE, PASSING` 
+
